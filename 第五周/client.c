85335df96c1afd81b2fd8f0f79dc3120a25e85ed
zmw <844833466@qq.com>
2017-05-06 12:09:23 +0800

    add two files

--- 第五周/client.c
+++ 第五周/client.c
@@ -11,12 +11,13 @@
 #define LOCALADDR "/home/zmw/文档/第五周/1.socket"
 int main()
 {
+	int ret;
 	int client_fd;
 	int server_len;
 	int client_connect;
 	struct sockaddr_un server_addr;
-	char *buffer="hello zmw";
-	char buffer1[50];
+	char *buffer="hello";
+	char buffer1[1024];
 	client_fd=socket(PF_UNIX,SOCK_STREAM,0);
 	server_addr.sun_family=PF_UNIX;
 	strcpy(server_addr.sun_path,LOCALADDR);
@@ -30,10 +31,16 @@ int main()
 		return -1;
 	}
         printf("连接成功\n");
-	write(client_fd,buffer,strlen(buffer));
-	read(client_fd,buffer1,50);
+while(1)
+{
+	printf("please input:");
+	gets(buffer1);
+	write(client_fd,buffer1,strlen(buffer1));
+	memset(buffer1,'\0',1024);
+	ret=read(client_fd,buffer1,1024);
+	buffer1[ret]='\0';
         printf("from server:%s\n",buffer1);
-	close(client_fd);
-	close(client_fd);
-	exit(0);
+	memset(buffer1,'\0',1024);
+}
+return 0;
 }
--- 第五周/server.c
+++ 第五周/server.c
@@ -10,120 +10,131 @@
 #include<sys/un.h>
 #include<sys/time.h>
 #include<sys/select.h>
+#define SIZE 1024
+#define SIZEADD 2048
 #define LOCALADDR "/home/zmw/文档/第五周/1.socket"
+struct sockaddr_un sock_server;
+struct sockaddr_un sock_client;
+int fds[64];
 int main()
-{
-	int fd[5];
-	int count;
-	int isREUSE=1;
-	int sock_opt;
-	int server_fd,client_fd;
-	int server_len,client_len;
-	int server_bind;
-	int server_listen;
-	struct sockaddr_un saddr_s;
-	struct sockaddr_un saddr_c;
-	int rdsocketlen=0;	
-
- 	server_fd=socket(PF_UNIX,SOCK_STREAM,0);
-	if(server_fd==-1)
+{	int server_fd,client_fd;
+	int server_len=sizeof(sock_server);
+	int client_len=sizeof(sock_client);
+	int sock_bind;
+	int sock_listen;
+	char buf[SIZEADD];	
+	char *add="hello zmw";
+	int yes=1;
+	server_fd=socket(AF_UNIX,SOCK_STREAM,0);//创建域套接字
+	if(server_fd<0)
 	{
-		printf("socket创建失败！\n");
-		return -1;
-	}
-	sock_opt=setsockopt(server_fd,SOL_SOCKET,SO_REUSEADDR,&isREUSE,sizeof(isREUSE));//允许在同一个端口启动服务器的多个实例
-	if(sock_opt==-1)
+		printf("socket创建失败!\n");
+		exit(-1);
+	}	
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR,&yes, sizeof(int)) == -1) {  
+        perror("setsockopt");  
+        exit(1);  
+    }  
+     
+	sock_server.sun_family=AF_UNIX;//设置结构体参数
+	strcpy(sock_server.sun_path,LOCALADDR);
+	sock_bind=bind(server_fd,(struct sockaddr *)&sock_server,server_len);//绑定域套接字
+	if(sock_bind!=0)
 	{
-		printf("setsockopt失败\n");
+		printf("bind绑定失败!\n");
 		exit(-1);
 	}
-
-	saddr_s.sun_family=PF_UNIX;
-	strcpy(saddr_s.sun_path,LOCALADDR);
-	server_len=sizeof(saddr_s);
-	server_bind=bind(server_fd,(struct sockaddr *)&saddr_s,server_len);
-	if(server_bind!=0)
+	sock_listen=listen(server_fd,5);//设置监听
+	if(sock_listen<0)
 	{
-		printf("bind绑定失败！\n");
-		return -1;
+		printf("监听创建失败！\n");
+		exit(-1);
 	}
-	server_listen=listen(server_fd,3);
-	if(server_listen<0)
-	{
-		printf("listen建立队列失败！\n");
-		return -1;
-	}
-	fd_set readfds;
-	struct timeval time_a;
-	int maxfd;
-	maxfd=server_fd;
-	count=0;
-/*	while(1)
-	{
-		printf("等待客户请求到达......\n");
-		int server_accept;
-		client_len=sizeof(saddr_c);
-		client_fd=accept(server_fd,(struct sockaddr *)&saddr_c,&client_len);
-		if(client_fd<0)
-		{
-			printf("accept建立连接失败！\n");
-			return -1;
-		}
-		printf("连接成功\n");
-		char *add="recieved";
-		char buf[50];
-		read(client_fd,buf,50);
-		printf("server read %s\n",buf);
-		strcat(buf,add);
-		printf("server add %s\n",buf);
-		write(client_fd,buf,50);
-		close(client_fd);
-	}*/
-char *add="recieved";
-char buf[50];
 	while(1)
 	{
-		FD_ZERO(&readfds);
-		FD_SET(server_fd,&readfds);
-		time_a.sec=10;
-		time_a.usec=0;
-		maxfd=server_fd;
-		for(int i=0;i<count;i++)
+		fd_set read_t;//需要监视的可读文件描述符集合
+		fd_set write_t;//需要监视的可写文件描述符集合
+		int fds_max;//描述符中最大的
+		int num=sizeof(fds)/sizeof(fds[0]);
+		for(int i=0;i<num;i++)
 		{
-			FD_SET(fd[i],&readfds);
-			if(fd[i]>maxfd)
+			fds[i]=-1;//初始化描述符集合中的所有描述符
+		}		
+		fds[0]=server_fd;//设置第一个描述符为域套接字服务器描述符
+		fds_max=server_fd;//设置初始描述符最大值;
+		struct timeval times;//用于描述一段时间长度，在这个时间内，需要监视的描述符没有时间发生则返回0
+		while(1)
+		{
+			FD_ZERO(&read_t);
+			FD_ZERO(&write_t);//因为这两个参数为输入型参数，一直会改变，每次循环都需要初始化
+			FD_SET(server_fd,&read_t);//把服务器描述符置入可读文件描述符集合
+			times.tv_sec=10;//设置10秒
+			times.tv_usec=0;//设置毫秒为0
+			for(int i=1;i<num;i++)//在select之前把所有的文件描述符都设置成读事件
 			{
-				maxfd=fd[i];
+				if(fds[i]>0)
+				{
+					FD_SET(fds[i],&read_t);
+					if(fds[i]>fds_max)
+					{
+						fds_max=fds[i];//设置最大描述符				
+					}
+				}
+			}
+			int select_t;
+			select_t=select(fds_max+1,&read_t,&write_t,NULL,&times);//调用select函数
+			switch(select_t)
+			{
+				case 0:
+					printf("time out ...\n");
+					break;
+				case -1:
+					printf("select失败!\n");
+					break;
+				default:
+					for(int i=0;i<num;i++)
+					{
+						if((fds[i]==server_fd)&&(FD_ISSET(fds[i],&read_t)))//测试是否可读文件
+							{
+				       printf("正在等待连接....\n");								
+				client_fd=accept(server_fd,(struct sockaddr *)&sock_client,&client_len);//等待连接请求
+								
+								if(client_fd<0)
+								{
+									printf("accept失败！\n");
+									continue;	
+								}
+								printf("新链接创建，描述符为%d\n",client_fd);
+								for(int i=0;i<num;i++)
+								{
+									if(fds[i]<0)
+									{
+										fds[i]=client_fd;
+										FD_SET(fds[i],&write_t);
+										break;
+									}
+								}
+								if(i==num-1)
+								{
+									printf("文件描述符已满，请关闭部分链接\n");	
+								}
+							}
+						else if((fds[i]>0)&&(FD_ISSET(fds[i],&read_t)))//检查该描述符是不是在读中，若在则读
+						{	
+							memset(buf,'\0',SIZE);
+							read(fds[i],buf,SIZE);
+							printf("接收到的数据为%s\n",buf);
+							strcat(buf,add);
+							printf("add后的数据为%s\n",buf);
+							write(fds[i],buf,80);
+							//memset(buf,'\0',SIZEADD);
+							
+						}
+					}break;
 			}
 		}
-		int select_a=select(maxfd+1,&readfds,NULL,NULL,&time_a);
-		if(select_a<0)
-		{
-			printf("select失败\n");
-		}
-		if(select_a==0)
-		{
-			continue;
-		}
-	for(int j=0;j<count;j++)
-	{
-		if(FD_ISSET(fd[j],&readfds))
-		{
-			rdsocketlen=read(select[j],buf,50);
-			printf("server read %s\n",buf);
-			strcat(buf,add);
-			write(select[j],buf,50);
-			if(rdsocketlen>0)
-			{
-			printf("server add %s\n",buf);
-			}
-		}
-	
 	}
-	
 
-	}
-	return 0;
 }
-//server.c
+//server.c 参考http://10704527.blog.51cto.com/10694527/1782636
 
